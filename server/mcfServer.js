781e2f477bd335c9c359ce7041bb26fb0b921c8b
rishiekugathasan <rishie2001@gmail.com>
2021-05-16 15:39:27 -0400

    Update mcfServer.js

--- server/mcfServer.js
+++ server/mcfServer.js
@@ -11,6 +11,22 @@ app.get('/',function(req,res) {
     res.send("Home Page.");
 });
 
+app.get('/aboutUs',function(req,res) {
+    res.sendFile('./aboutUs.html',{root:__dirname});
+});
+
+app.get('/whoweare',function(req,res) {
+    res.sendFile('./whoweare.html',{root:__dirname});
+});
+
+app.get('howItWorks',function(req,res) {
+    res.sendFile('./howItWorks.html',{root:__dirname});
+});
+
+app.get('meetTheTeam',function(req,res) {
+    res.sendFile('./meetTheTeam.html',{root:__dirname});
+});
+
 mc.connect("mongodb://localhost:27017",function(err,client) {
     if (err) {
         console.log("Error connecting to database.");

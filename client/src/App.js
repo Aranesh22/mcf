3f6ee70f70b6842c9bfca907ae46a73c3fbcfb09
Lindiks <lindsay.dickson@outlook.com>
2021-05-20 18:06:45 -0400

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import Coaches from './components/Coaches'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Coaches />
+
+    </div>
+  );
+}
+
+export default App;

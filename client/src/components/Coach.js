d8eea1bbfc6e8e9b1ea9d4c22543b442f7fa44f1
Lindiks <lindsay.dickson@outlook.com>
2021-05-20 18:06:54 -0400

    Create Coach.js

---
+++ client/src/components/Coach.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Card} from 'react-bootstrap'
+import 'holderjs'
+
+function Coach({name, imgurl, bio }) {
+
+    return (
+
+        <Card className="card slide-in-from-right " style={{ 'min-width': '15rem',  margin:"0.5rem" }} >
+            <Card.Img variant="top" src={imgurl} onError={(e)=>{e.target.onerror = null; e.target.src="holder.js/100px180?text=Image cap"}}/>
+            <Card.Body>
+                <Card.Title>{name}</Card.Title>
+                <Card.Text>{bio}</Card.Text>
+            </Card.Body>
+        </Card>
+
+      
+    )
+}
+
+export default Coach

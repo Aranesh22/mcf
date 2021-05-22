eb8638686c68a7a2c9fbe1e7771aaf940bedec59
Lindiks <lindsay.dickson@outlook.com>
2021-05-22 19:41:07 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './App.css';
 
 import Coaches from './components/Coaches'
-
+import Timeline from './components/Timeline'
 
 function App() {
   return (
     <div className="App">
-      <Coaches />
-
+      {/* <Coaches /> */}
+      <Timeline />
     </div>
   );
 }

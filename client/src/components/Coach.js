c3aa8c74807f0627cacb5862b9f70d7654fdd20e
Lindiks <lindsay.dickson@outlook.com>
2021-05-22 19:31:42 -0400

    Update Coach.js

--- client/src/components/Coach.js
+++ client/src/components/Coach.js
@@ -6,7 +6,7 @@ function Coach({name, imgurl, bio }) {
 
     return (
 
-        <Card className="card slide-in-from-right " style={{ 'min-width': '15rem',  margin:"0.5rem", 'max-width': '15rem'}} >
+        <Card className="card slide-in-from-right " style={{ minWidth: '15rem',  margin:"0.5rem", maxWidth: '15rem'}} >
             <Card.Img variant="top" src={imgurl} onError={(e)=>{e.target.onerror = null; e.target.src="holder.js/100px180?text=Image cap"}}/>
             <Card.Body>
                 <Card.Title>{name}</Card.Title>

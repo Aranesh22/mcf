f9cd3a596be9a318101ff4bd281117fffa7435f6
rishiekugathasan <rishie2001@gmail.com>
2021-05-28 18:41:24 -0400

    Update aboutUs.js

--- server/routes/aboutUs.js
+++ server/routes/aboutUs.js
@@ -1,5 +1,6 @@
 const express = require('express');
 let router = express.Router();
+var nodemailer = require('nodemailer');
 
 router.get('/',function(req,res) {
     res.sendFile('/dummyPages/aboutUs.html',{root:'../server'});
@@ -9,6 +10,29 @@ router.get('/',function(req,res) {
 router.get('/meetTheTeam',function(req,res) {
     res.sendFile('/dummyPages/meetTheTeam.html',{root:'../server'});
     //res.send("hi get /aboutUs/meetTheTeam.");
+});
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'getrekt.soccer@gmail.com',
+        pass: '20RishiE01'
+    }
+});
+
+var mailOptions = {
+    from: 'getrekt.soccer@gmail.com',
+    to: 'rishie2001@gmail.com',
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!'
+};
+  
+transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+        console.log(error);
+    }else {
+        console.log('Email sent: ' + info.response);
+    }
 });
 
 module.exports = router;

d72dbc062a677f3181a6e2342fa37a6489e77691
Lindiks <lindsay.dickson@outlook.com>
2021-05-31 14:11:56 -0400

    Create HexagonGroup.js

---
+++ client/src/components/HexagonGroup.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import {Container} from 'react-bootstrap'
+import '../css/mockup.css'
+import faceOfFitness from '../assets/BKYJ9883.jpeg'
+
+export default function HexagonGroup() {
+    return (
+        <div>
+            <div className="hexagon-group">
+                <div class="hexagon" />
+                <div class="hexagon text-hexagon">
+                    <div class="milestoneTxt">
+                        <p>Sample text here</p>
+                    </div>
+                </div>  
+                <div class="hexagon year-hexagon">
+                    <div class="milestoneTxt">
+                        <h1>2021</h1>
+                    </div>
+                </div>
+            </div>
+
+            
+
+
+
+
+
+        </div>
+    )
+}

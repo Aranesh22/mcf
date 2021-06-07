29ba9b227a4c7b0047a463692ab710e7d7d41768
Lindiks <lindsay.dickson@outlook.com>
2021-06-07 00:19:48 -0400

    Create Homepage.js

---
+++ client/src/components/Homepage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../css/Homepage.css'
+import TrainPanel from './PanelsHomepage/TrainPanel'
+
+export default function Homepage() {
+    // NOTE: haven't yet focused on how the UI will scale 
+    return (
+        <TrainPanel/>
+        
+    )
+}

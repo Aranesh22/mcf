3b261ffd30cd82999e481ae73e77a667e633890f
Lindiks <lindsay.dickson@outlook.com>
2021-06-12 17:21:35 -0400

    Update Homepage.js

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import '../css/Homepage.css'
 import TrainPanel from './PanelsHomepage/TrainPanel'
-
+import ReviewPanel from './PanelsHomepage/ReviewPanel'
+import Nav from './Navbar/Navbar'
 export default function Homepage() {
     // NOTE: haven't yet focused on how the UI will scale 
     return (
+        <>
+        <Nav />
         <TrainPanel/>
+        <ReviewPanel />
+        </>
         
     )
 }

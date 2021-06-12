0561fabf2302ea3ec195e01361e8f7a782c01ebe
HumptyDu <shadyashraf2002@gmail.com>
2021-06-11 22:27:03 -0400

    Create MenuItems.js

---
+++ client/Navbar/MenuItems.js
@@ -0,0 +1,32 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'About Us',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        img: './logo.png',
+        url: '#',
+        cName:'nav-img'
+    },
+    {
+        title: 'Plans',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact Us',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Mobile',
+        url: '#',
+        cName: 'nav-links-mobile'
+    }
+]

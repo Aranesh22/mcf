b61e9a18d9039303a1d53c7203249b56ea4c6556
Aranesh22 <a.aranesh@gmail.com>
2021-06-12 17:07:55 -0400

    Update TrainPanel.js

--- client/src/components/PanelsHomepage/TrainPanel.js
+++ client/src/components/PanelsHomepage/TrainPanel.js
@@ -10,11 +10,12 @@ export default function TrainPanel() {
             <div className="bgDiv">
                 <img style={{width: '95%', marginTop: '-16%'}} src={trainLogo} />
                 <div className="flexAlignEnd d-flex" style={{padding: '4%'}}>
-                <Button className="buttonWhite">FIND YOUR PLAN</Button> </div>
-                <div className='txtBox'>
-                    {/* <p >MyCure Fitness is a team of physical strength optimization coaches, nutritionists, physical therapy specialist, internal health physicians.  We believe that with our experience of working with 1000’s of clients both in Canada and worldwide we have filtered a contemporary program that will give every individual a proprietary program that will address and optimize strength through individualized workout coaching, nutrition coach, physician supervised health assessment and prehab and rehab accelerations with our in house team of physical therapists</p> */}
-                    <VSlider />
-                </div>
+                <Button className="buttonWhite">FIND YOUR PLAN</Button> </div> 
+                
+                {/* <div className='txtBox'>
+
+                </div>   */}
+                <VSlider />
                 <div className="flexAlignEnd d-flex">
                    <h1 className="mcfH1">WE ARE MYCUREFITNESS</h1>
                                

532bb5c6ac9629da9fffbad219a6ada0ae3027c9
Aranesh22 <a.aranesh@gmail.com>
2021-06-12 16:00:45 -0400

    Create autoVSlider.js

---
+++ client/src/components/autoVSlider.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './styleOne.css';
+
+class vSlider extends Component {
+   
+    render(){
+        return(
+            <div class ="content">   
+                <div class ="slider-wrapper">
+                Aranesh
+                <div class ="slider"> 
+                    <div class="slider-text1">single</div> 
+                    <div class="slider-text2">longer</div> 
+                    <div class="slider-text3">no</div>   
+                    <div class="slider-text4">is</div>
+                </div>
+                </div>   
+            </div>
+        )
+    }
+}
+
+export default vSlider

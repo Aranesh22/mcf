6e567b500e1e2b470a366fdd72d5d503ee17b84a
Lindiks <lindsay.dickson@outlook.com>
2021-06-13 17:34:21 -0400

    Update Navbar.js

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {MenuItems} from "./MenuItems";
 import '../../css/Navbar.css';
 
+
 class Navbar extends Component {
     state = { clicked: false}
 

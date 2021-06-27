349f936a66925daeee1e688ff162a7312bf1d369
Lindiks <lindsay.dickson@outlook.com>
2021-06-27 15:11:05 -0400

    Create ReviewCardLanding.js

---
+++ client/src/components/LandingPages/ReviewCardLanding.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Card} from 'react-bootstrap'
+// import '../../css/Homepage.css'
+
+
+export default function ReviewCardLanding({txt, author}) {
+    return (
+       
+         <Card className="reviewCard">
+             <Card.Body>
+                <Card.Text>
+                    {txt}
+                </Card.Text>
+             </Card.Body>
+             <footer className="blockquote-footer">
+                <i>{author}</i>
+            </footer>
+         </Card>
+        
+    )
+}

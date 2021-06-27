0399da931f6452b1de276d72535bdf178f9dd3c2
Lindiks <lindsay.dickson@outlook.com>
2021-06-27 15:11:07 -0400

    Create HomepageLanding.js

---
+++ client/src/components/LandingPages/HomepageLanding.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../../css/Landing.css'
+import ReviewsLanding from './ReviewsLanding';
+import TrainLanding from './TrainLanding';
+
+
+export default function HomepageLanding() {
+    return (
+        <>
+        <TrainLanding />
+        <ReviewsLanding />
+        </>
+        
+    )
+}

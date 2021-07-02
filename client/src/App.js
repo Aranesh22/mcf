6c1682fa925e887d71675d84c35a7e85dbbfec02
Lindiks <lindsay.dickson@outlook.com>
2021-07-02 18:40:08 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 
 // import Homepage from './components/Homepage';
 import HomepageLanding from './components/LandingPages/HomepageLanding';
+import LinksLanding from './components/LandingPages/LinksLanding';
 
 function App() {
   return (
     <div className="App">
       {/* <Homepage /> */}
-      
+
+
+      {/* <LinksLanding /> */}
       <HomepageLanding />
     
     </div>

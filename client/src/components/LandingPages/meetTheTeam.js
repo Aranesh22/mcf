0a7e69d63fe1984a34543207f7aa4f028dcf7c86
Lindiks <lindsay.dickson@outlook.com>
2021-07-02 20:11:07 -0400

    Update meetTheTeam.js

--- client/src/components/LandingPages/meetTheTeam.js
+++ client/src/components/LandingPages/meetTheTeam.js
@@ -2,17 +2,22 @@ import React from 'react'
 import backgroundImage from '../../assets/BackGroundImage.svg'
 import AA from '../../assets/aATransparent.svg'
 import mcf from '../../assets/mcf.svg'
-// will need to import the background image + the 2 other logos for clickable links  
+import t1 from '../../assets/mTimages/DPP_HP.svg'
+import wH from '../../assets/mTimages/whiteHexPannel.svg'
+import t3 from '../../assets/mTimages/RS_HP.svg'
+import t4 from '../../assets/mTimages/JG_HP.svg'
+
+
 import '../../css/lpmeetTheTeam.css'
 
 export default function LinksLanding() {
     return (
         <div class="containTeam">  
-            <img class ="testone" src="./client/src/assets/mTimages/DPP_HP.svg"alt=""></img>
-            <img class="testTwo" src="./client/src/assets/mTimages/whiteHexPannel.svg" alt=""></img>
-            <img class="testThree" src="./client/src/assets/mTimages/RS_HP.svg" alt=""></img>
-            <img class="testFour" src="./client/src/assets/mTimages/JG_HP.svg"alt=""></img>  
-            <img class="testFive" src="./client/src/assets/mTimages/whiteHexPannel.svg" alt=""></img>
+            <img class ="testone" src={t1} alt={wH}></img>
+            <img class="testTwo" src={wH} alt={wH}></img>
+            <img class="testThree" src={t3} alt={wH}></img>
+            <img class="testFour" src={t4} alt={wH}></img>  
+            <img class="testFive" src={wH} alt={wH}></img>
         </div>
     )
 }

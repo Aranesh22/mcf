d62e33dcdcd7244ac1c999cd26b02c04bc337336
Lindiks <lindsay.dickson@outlook.com>
2021-07-02 20:10:50 -0400

    Update HomepageLanding.js

--- client/src/components/LandingPages/HomepageLanding.js
+++ client/src/components/LandingPages/HomepageLanding.js
@@ -2,13 +2,14 @@ import React from 'react'
 import '../../css/Landing.css'
 import ReviewsLanding from './ReviewsLanding';
 import TrainLanding from './TrainLanding';
-
+import MeetTheTeam from './meetTheTeam';
 
 export default function HomepageLanding() {
     return (
         <div style={{position: 'absolute', top:'-10%'}}>
         <TrainLanding />
         <ReviewsLanding />
+        <MeetTheTeam />
         </div>
         
     )

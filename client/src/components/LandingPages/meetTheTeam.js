51fb7092dfa2d9ba38d8a2ee9b8c67fe94f96f6a
Aranesh22 <a.aranesh@gmail.com>
2021-07-04 00:12:46 -0400

    Update meetTheTeam.js

--- client/src/components/LandingPages/meetTheTeam.js
+++ client/src/components/LandingPages/meetTheTeam.js
@@ -3,8 +3,8 @@ import backgroundImage from '../../assets/BackGroundImage.svg'
 import AA from '../../assets/aATransparent.svg'
 import mcf from '../../assets/mcf.svg'
 import t1 from '../../assets/mTimages/DPP_HP.svg'
-import wH from '../../assets/mTimages/whiteHexPannel.svg'
-import t3 from '../../assets/mTimages/RS_HP.svg'
+import wH from '../../assets/mTimages/wH.svg'
+import t3 from '../../assets/mTimages/RS_HP (1).svg'
 import t4 from '../../assets/mTimages/JG_HP.svg'
 
 
@@ -13,10 +13,10 @@ import '../../css/lpmeetTheTeam.css'
 export default function LinksLanding() {
     return (
         <div class="containTeam">  
-            <img class ="testone" src={t1} alt={wH}></img>
+            <img class ="testone" src={t3} alt={wH}></img>
             <img class="testTwo" src={wH} alt={wH}></img>
             <img class="testThree" src={t3} alt={wH}></img>
-            <img class="testFour" src={t4} alt={wH}></img>  
+            <img class="testFour" src={t3} alt={wH}></img>  
             <img class="testFive" src={wH} alt={wH}></img>
         </div>
     )

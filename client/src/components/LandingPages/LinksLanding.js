0ce4e47056a7997d9a58cee6fbacf99fb6cb4751
Lindiks <lindsay.dickson@outlook.com>
2021-07-06 15:01:30 -0400

    Update LinksLanding.js

--- client/src/components/LandingPages/LinksLanding.js
+++ client/src/components/LandingPages/LinksLanding.js
@@ -8,15 +8,14 @@ export default function LinksLanding() {
         <div style={{ backgroundImage: `url(${backgroundImage})`, height:'100vh', overflow:'hidden', backgroundSize: 'cover'}}>
             <div  className="centerContainer" >
             <div className="linksContainer" >
-                   <a href="https://www.athleticadvantage.ca/"> <img style={{maxWidth:'50%', padding:'2% 0%'}} src={AA} >
-                 </img></a>
-                 
+                  <a href="https://www.athleticadvantage.ca/"> <img style={{maxWidth:'50%', padding:'2% 0%'}} src={AA} >
+                 </img></a> 
             </div>
-            <div className="linksContainer" >
+            <div className="linksContainer">
                 <a href="mycurefitnessURLHERE"> 
-                <img style={{maxWidth:'45%',  padding:'2% 0%'}} src={mcf} ></img></a>
-                </div> 
-                </div>
+                <img style={{maxWidth:'45%',  padding:'2% 0%'}} src={mcf}></img></a>
+              </div> 
+            </div>
             <style>{"\
         .body{\
           color:red;\

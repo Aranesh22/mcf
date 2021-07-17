3753b9d0f09ac6bdc6c2ddb9212945b7c73437ea
Lindiks <lindsay.dickson@outlook.com>
2021-07-17 17:10:13 -0400

    Update meetTheTeam.js

--- client/src/components/LandingPages/meetTheTeam.js
+++ client/src/components/LandingPages/meetTheTeam.js
@@ -1,11 +1,9 @@
 import React from 'react' 
-import backgroundImage from '../../assets/BackGroundImage.svg'
-import AA from '../../assets/aATransparent.svg'
-import mcf from '../../assets/mcf.svg'
-import t1 from '../../assets/mTimages/DPP_HP.svg'
+
+import t1 from '../../assets/mTimages/updated/DPP_HP.svg'
 import wH from '../../assets/mTimages/wH.svg'
 import t3 from '../../assets/mTimages/RS_HP (1).svg'
-import t4 from '../../assets/mTimages/JG_HP.svg'
+import t4 from '../../assets/mTimages/updated/JG_HP.svg'
 
 
 import '../../css/lpmeetTheTeam.css'
@@ -13,11 +11,11 @@ import '../../css/lpmeetTheTeam.css'
 export default function MeetTheTeam() {
     return (
         <div class="containTeam">  
-            <img class ="testone" src={t3} alt={wH}></img>
-            <img class="testTwo" src={wH} alt={wH}></img>
-            <img class="testThree" src={t3} alt={wH}></img>
-            <img class="testFour" src={t3} alt={wH}></img>  
-            <img class="testFive" src={wH} alt={wH}></img>
+            <img class ="topLeft" src={t3} alt={wH}></img>
+            <img class="mid" src={t4} alt={wH}></img>
+            <img class="topRight" src={t1} alt={wH}></img>
+            <img class="bottomLeft" src={wH} alt={wH}></img>  
+            <img class="bottomRight" src={wH} alt={wH}></img>
         </div>
     )
 }

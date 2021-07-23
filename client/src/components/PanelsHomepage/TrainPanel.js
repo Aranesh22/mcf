06a8be4ef3815365dc3f242ede8b55c82b71f364
Lindiks <lindsay.dickson@outlook.com>
2021-07-23 15:22:16 -0400

    Update TrainPanel.js

--- client/src/components/PanelsHomepage/TrainPanel.js
+++ client/src/components/PanelsHomepage/TrainPanel.js
@@ -1,29 +1,31 @@
 import React from 'react'
-import {Button, Container} from 'react-bootstrap'
-import '../../css/Homepage.css'
 import trainLogo from '../../assets/train.png'
-import VSlider from '../util/autoVSlider'
-
+import {Button} from 'react-bootstrap'
 export default function TrainPanel() {
     return (
         <div className="trainTxtPanel d-flex flex-column">
             <div className="bgDiv">
                 <img style={{width: '95%', marginTop: '-16%'}} src={trainLogo} />
-                <div className="flexAlignEnd d-flex" style={{padding: '4%'}}>
-                <Button className="buttonWhite">FIND YOUR PLAN</Button> </div> 
+                <div className="flexAlignEnd d-flex" style={{padding: '4%'}}></div> 
                 
-                {/* <div className='txtBox'>
-
-                </div>   */}
-                <VSlider />
-                <div className="flexAlignEnd d-flex">
-                   <h1 className="mcfH1">WE ARE MYCUREFITNESS</h1>
-                               
+                <div className='txtBox'>
+                    <p >" Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis faucibus lorem sed elit tincidunt pretium. Duis molestie ultrices nisl, ut fringilla lacus imperdiet at. Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
+                    <footer className="blockquote-footer">
+                        <i>Quote </i>
+                    </footer>
+                    
+                    <div className="flexAlignStart d-flex" style={{padding: '4%'}}>
+                    <Button className="buttonWhite">Know Your Strength ⟶</Button> </div> 
                 </div>
+
+                {/* <div className="flexAlignEnd d-flex">
+                   <h1 className="mcfH1">WE ARE MYCUREFITNESS</h1>                  
+                </div> */}
             </div>
-            <div className='flexAlignEnd d-flex'>
-            <h1 className="mcfH1"style={{color: '#8B0808'}}>WE ARE MYCUREFITNESS</h1>
-           </div>
+            {/* <div style={{height:'50px'}}></div> */}
+            {/* <div className='flexAlignEnd d-flex'>
+                <h1 className="mcfH1"style={{color: '#004c54'}}>WE ARE MYCUREFITNESS</h1>
+           </div> */}
         </div>
     )
 }

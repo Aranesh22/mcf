e2aaed6bf4378794df94b8106263beb170038fb1
Lindiks <lindsay.dickson@outlook.com>
2021-07-27 22:01:29 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import './App.css';
-
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 
 import Homepage from './components/PanelsHomepage/Homepage';
 import LinksLanding from './components/LandingPages/LinksLanding';
+import Navbar from './components/Navbar/Navbar';
+// import meetTheTeam from './components/LandingPages/meetTheTeam'
+import MeetTheTeam from './components/LandingPages/meetTheTeam';
 
 
 function App() {
   return (
-    <div className="App">
-      <Homepage/>    
+    <div className="App"
+    >
+    
+      <Router>
+ 
+         <Navbar />
+
+        <Switch>
+          <Route exact path="/">
+            <Homepage />
+          </Route>
+          <Route path="/about">
+            <MeetTheTeam />
+          </Route>
+        </Switch>
+
+      </Router>
+     
+  
     </div>
   );
 }

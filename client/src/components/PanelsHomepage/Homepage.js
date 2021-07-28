fc2b0b2dbe7569604946be98c4bff7da81f5a175
Lindiks <lindsay.dickson@outlook.com>
2021-07-27 22:01:43 -0400

    Update Homepage.js

--- client/src/components/PanelsHomepage/Homepage.js
+++ client/src/components/PanelsHomepage/Homepage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../../css/Landing.css'
 
-
+import tripleHex from '../../assets/tripleHex.svg'
 
 import TrainPanel from './TrainPanel';
 
@@ -9,11 +9,13 @@ import ReviewCarousel from './ReviewCarousel';
 import HowitWorksPanel from './howitWorksPanel/HowitWorksPanel';
 import Faqq from './FaqPanel/Faq';
 import StickyButton from '../Navbar/StickyButton';
+import Navbar from '../Navbar/Navbar';
 
 
 export default function Homepage() {
     return (
         <div>
+           {/* <Navbar /> */}
            <StickyButton />
          <TrainPanel />
          <div className="flexAlignStart d-flex" style={{paddingLeft: '2%'}}>
@@ -23,7 +25,9 @@ export default function Homepage() {
         <div className='flexAlignEnd d-flex'>
                 <h1 className="mcfH1"style={{color: '#004c54'}}>WE ARE MYCUREFITNESS</h1>
            </div>
-
+         <div>
+         <img src={tripleHex} />
+         </div>
          <div className="flexAlignStart d-flex" style={{paddingLeft: '2%', backgroundColor: '#004c54'}}>
             <h1 className="mcfH1"style={{ padding: "2%", }}>HOW IT WORKS</h1>
          </div>

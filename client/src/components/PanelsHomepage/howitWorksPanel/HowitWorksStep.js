46b9340930806b9c6f64a677a833be09753118ab
Lindiks <lindsay.dickson@outlook.com>
2021-07-27 22:01:18 -0400

    Update HowitWorksStep.js

--- client/src/components/PanelsHomepage/howitWorksPanel/HowitWorksStep.js
+++ client/src/components/PanelsHomepage/howitWorksPanel/HowitWorksStep.js
@@ -2,16 +2,18 @@ import React from 'react'
 import '../../../css/howitWorks.css'
 import hWHex from '../../../assets/hWimgs/hWHex.svg'
 
-export default function HowitWorksStep({bgImg, numImg, text}) {
+export default function HowitWorksStep({bgImg, numImg, hText, text}) {
     return (
         <div className="containStep hWBg" style={{ backgroundImage: `url(${bgImg})`}}>
-            <div className="longHexContainer">
-            <img className="longHex" src={hWHex} ></img> 
-            <p className="hexTxt">{text} </p>
+            <div className="longHexContainer longHex" style={{backgroundImage: `url(${hWHex})`}}>
+            {/* <img className="longHex" src={hWHex} ></img>  */}
+            <div className="hexTxtBox" >
+            <h1 className="hexH1Text flexalignStart mcfH1"> {hText}</h1>
+            <p className="hexPText">{text} </p></div>
           </div>
             <div className="numberImgContainer">
             <img className="numberImg" src={numImg}></img></div>
         </div>
     )
 }
- 
+  

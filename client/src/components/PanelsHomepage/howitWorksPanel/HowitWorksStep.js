e3e4f5fe347d4d731dc15bb345353cbeecf0db0d
Lindiks <lindsay.dickson@outlook.com>
2021-07-31 19:18:48 -0400

    Update HowitWorksStep.js

--- client/src/components/PanelsHomepage/howitWorksPanel/HowitWorksStep.js
+++ client/src/components/PanelsHomepage/howitWorksPanel/HowitWorksStep.js
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../../../css/howitWorks.css'
 import hWHex from '../../../assets/hWimgs/hWHex.svg'
 
+import HowitWorksModal from './HowitWorksModal'
+import { Button } from 'react-bootstrap'
+
+
 export default function HowitWorksStep({bgImg, numImg, hText, text}) {
+    const [modalShow, setModalShow] = useState(false);
+
     return (
         <div className="containStep hWBg" style={{ backgroundImage: `url(${bgImg})`}}>
             <div className="longHexContainer longHex" style={{backgroundImage: `url(${hWHex})`}}>
-            {/* <img className="longHex" src={hWHex} ></img>  */}
-            <div className="hexTxtBox" >
-            <h1 className="hexH1Text flexalignStart mcfH1"> {hText}</h1>
-            <p className="hexPText">{text} </p></div>
+            <div className="hexTxtBox">
+            <h1 className="hexH1Text flexalignStart mcfH1">{hText}</h1>
+
+            <Button className="seeMoreBtn" variant="custom" onClick={() => setModalShow(true)}>
+              see more...
+            </Button>
+
+        <HowitWorksModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+        text = {text}
+        header = {hText}
+        />
+
+            </div>
           </div>
             <div className="numberImgContainer">
             <img className="numberImg" src={numImg}></img></div>

9512ab9072900c98ff0f93c5710d54d1043670ff
Lindiks <lindsay.dickson@outlook.com>
2021-07-31 19:18:47 -0400

    Update HowitWorksPanel.js

--- client/src/components/PanelsHomepage/howitWorksPanel/HowitWorksPanel.js
+++ client/src/components/PanelsHomepage/howitWorksPanel/HowitWorksPanel.js
@@ -9,14 +9,14 @@ import no4 from '../../../assets/hWimgs/no4.svg'
 import hWBg from '../../../assets/hWimgs/hWBg.png'
 
 const stepInfo = [
-    {bgImg: hWBg, numImg: no1, hText: "Assess", text: "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident."},
-    {bgImg: hWBg, numImg: no2, hText: "Execute", text: "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident"},
-    {bgImg: hWBg, numImg: no3, hText: "Reassess", text: "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident"},
-    {bgImg: hWBg, numImg: no4, hText: "Progress", text: "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident"}
-
+    {bgImg: hWBg, numImg: no1, hText: "Assess", text: 'Start here, Know your strength Sit strong and a strength and optimization expert will give you a call to discuss your next move, after a comprehensive intake that will give the clearest picture of where you are in your journey and the vision to carry it to success'},
+    {bgImg: hWBg, numImg: no2, hText: "Execute", text: 'Programming that reflects your strength, health, nutrition, and cognitive behavioural enhancement is rolled out This is according to the intake'},
+    {bgImg: hWBg, numImg: no3, hText: "Reassess your potential", text: 'Feedback is a paramount here'},
+    {bgImg: hWBg, numImg: no4, hText: "Automate your strength", text: 'With the tools in place to ensure a habit shift and a physical empowerment, its you back in the driver seat, with the tools your need to succeed, don’t worry, we are always here to ensure you stay the course and level up as you an athlete for life'}
 ]
+
 const renderStep = (step, index) => {
-    return(
+    return (
       <HowitWorksStep key={index}
             bgImg={step.bgImg}
             numImg={step.numImg}

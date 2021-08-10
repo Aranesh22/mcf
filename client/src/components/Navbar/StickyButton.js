8366853d8662b043138d9a689111c49edad06e94
Lindiks <lindsay.dickson@outlook.com>
2021-08-10 18:50:53 -0400

    Update StickyButton.js

--- client/src/components/Navbar/StickyButton.js
+++ client/src/components/Navbar/StickyButton.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Button } from 'react-bootstrap'
-import '../../css/sticky.css'
+import '../../css/main.css'
 
 export default function StickyButton() {
     return (

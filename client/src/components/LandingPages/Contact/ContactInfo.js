bf02ca708c986eedad8beb3f4eff92fb9e47704c
Lindiks <lindsay.dickson@outlook.com>
2021-08-11 15:48:56 -0400

    Update ContactInfo.js

--- client/src/components/LandingPages/Contact/ContactInfo.js
+++ client/src/components/LandingPages/Contact/ContactInfo.js
@@ -10,12 +10,12 @@ export default function ContactInfo() {
                 {/* insert svg here */}
                 <img style={{height: '40px'}} src={phoneIcon} >
                  </img>
-                    <p className="px-4" style={{color: '#004c54'}}>416-756-9023</p >
+                    <a href="tel:###-###-####" className="linkTo px-4" style={{color: '#004c54'}}>416-756-9023</a >
                 </div>
                 <p  className="flexAlignStart d-flex px-4" style={{color: '#004c54'}}>Or just visit us!</p>
                 <div className="formInfoDiv d-flex flex-direction-row px-4">
                         <img style={{height: '40px'}} src={homeIcon}/>
-                        <p className="px-4" style={{color: '#004c54', textAlign: 'start'}}>26 MyStreet Dr. <br></br>L3S 3BD. Markham, ON</p>
+                        <a href="googlemapsLinkHere" className="linkTo px-4" style={{color: '#004c54', textAlign: 'start'}}>26 MyStreet Dr. <br></br>L3S 3BD. Markham, ON</a>
                 </div>
         
         </div>

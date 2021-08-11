a7562dec60acc967e7e794ad116314c66d4ced95
Lindiks <lindsay.dickson@outlook.com>
2021-08-11 15:20:34 -0400

    Update FaqQuestion.js

--- client/src/components/PanelsHomepage/FaqPanel/FaqQuestion.js
+++ client/src/components/PanelsHomepage/FaqPanel/FaqQuestion.js
@@ -1,19 +1,16 @@
 import React from 'react'
 import { Accordion } from 'react-bootstrap';
-
 import ScaleText from "react-scale-text";
 
 export default function FaqQuestion({ekey, question, answer }) { 
       const FaqQ = (props) => (
-        <ScaleText minFontSize={16} maxFontSize={28} >
+        <ScaleText minFontSize={16} maxFontSize={24} >
           <span style={{color: '#004c54', textAlign:"start", fontWeight:'600'}}>{props.question}</span>
         </ScaleText>
       );
     return (
       <>      
-        <Accordion.Toggle eventKey={ekey}  className="faqBtn" >    
-        
-  
+        <Accordion.Toggle eventKey={ekey}  className="faqBtn">      
             <h1 className="mcfH1" style={{color: '#004c54', textAlign:"start", fontWeight:'600'}}><FaqQ question={question} /></h1>
         </Accordion.Toggle>
             <Accordion.Collapse eventKey={ekey} >

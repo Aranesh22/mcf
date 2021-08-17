c7dc28d3dfae8525c19094cacc452d4f5bcd819b
Lindiks <lindsay.dickson@outlook.com>
2021-08-16 20:26:43 -0400

    Create AltPanel.js

---
+++ client/src/components/PanelsHomepage/AltPanel.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function AltPanel() {
+    return (
+        <div style={{background: '#004c54'}}>
+            <div className="px-5" style={{width: '45%', borderTop: 'solid white'}}>
+            Those that succeed in fitness have a coach, but we go further - our system provides you with the understanding and experience to become your own coach, for life.
+            </div>
+            <div className="px-5" style={{width: '45%', borderTop: 'solid white'}}>
+            Those that succeed in fitness have a coach, but we go further - our system provides you with the understanding and experience to become your own coach, for life.
+            </div>
+        </div>
+    )
+}

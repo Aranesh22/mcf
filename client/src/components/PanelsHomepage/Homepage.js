348c3ed916d61e09d09ea3a51d02120655f7851d
Lindiks <lindsay.dickson@outlook.com>
2021-08-16 20:26:53 -0400

    Update Homepage.js

--- client/src/components/PanelsHomepage/Homepage.js
+++ client/src/components/PanelsHomepage/Homepage.js
@@ -3,12 +3,13 @@ import React from 'react'
 import '../../css/Home.css'
 import '../../css/main.css'
 import tripleHex from '../../assets/tripleHex.svg'
-
 import TrainPanel from './TrainPanel';
 
 import ReviewCarousel from './ReviewCarousel';
 import HowitWorksPanel from './howitWorksPanel/HowitWorksPanel';
 import Faqq from './FaqPanel/Faq';
+import AltPanel from './AltPanel';
+import HexPanelText from './HexPanelText';
 
 
 export default function Homepage() {
@@ -24,7 +25,11 @@ export default function Homepage() {
                 <h1 className="mcfH1"style={{color: '#004c54'}}>WE ARE MYCUREFITNESS</h1>
            </div>
          <div>
-         <img src={tripleHex} />
+         <div className="flexAlignStart d-flex mt-5" style={{paddingLeft: '2%', backgroundColor: '#004c54'}}>
+            <h1 className="mcfH1"style={{ padding: "2%", }}>WHY YOU SUCCEED</h1>
+         </div>
+         {/* <AltPanel /> */}
+         <HexPanelText />
          </div>
          <div className="flexAlignStart d-flex" style={{paddingLeft: '2%', backgroundColor: '#004c54'}}>
             <h1 className="mcfH1"style={{ padding: "2%", }}>HOW IT WORKS</h1>
@@ -34,7 +39,7 @@ export default function Homepage() {
             <h1 className="mcfH1"style={{ padding: "2%" }}>FREQUENTLY ASKED QUESTIONS</h1>
          </div>
         <Faqq />
-        {/* <MeetTheTeam /> */}
+   
          </div>
 
         

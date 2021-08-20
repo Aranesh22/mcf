4579eece13630d1f41ffad36db3ea2565fa722a6
Lindiks <lindsay.dickson@outlook.com>
2021-08-20 19:33:59 -0400

    Update Navbar.js

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,44 +1,21 @@
-import React, { Component } from 'react';
-import {MenuItems} from "./MenuItems";
-import 'react-bootstrap';
-import '../../css/Navbar.css';
+import React from 'react'
+import Media from 'react-media'
 
+import NavDesktop from './NavDesktop'
+import NavMobile from './NavMobile'
 
-class Navbar extends Component {
-    state = { clicked: false}
-
-    handleClick = () => {
-        this.setState({clicked: !this.state.clicked});
-    }
-
-    render(){
-        return(
-            <nav className="NavbarItems">
-                <h1 className="navbar-logo">Somethinf<i className="fab fa-react"></i></h1>
-                <div className="menu-icon" onClick={this.handleClick}>
-                    <i  className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
-                </div>
-                <ul className = {this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
-                    {MenuItems.map((item, index) => {
-                        // if(index==2){
-                        //     return(
-                        //         <img src={item.img} href={item.url} alt={item.img} className={item.cName}></img>
-                        //     )
-                        // }
-                        
-                        return (
-                            <li key={index}>
-                                <a className={item.cName} href={item.url}>
-                                {item.title}
-                                </a>
-                            </li>
-                        )
-                    })}
-                </ul>
-            
-            </nav>
-        )
-    }
+export default function Navbar() {
+    return (
+        <div>
+           <Media queries={{ small: { maxWidth: 695 } }}>
+                        {matches =>
+                            matches.small ? (
+                            <NavMobile/>
+                            ) : (
+                                <NavDesktop />
+                            )
+                        }
+                    </Media>
+        </div>
+    )
 }
-
-export default Navbar

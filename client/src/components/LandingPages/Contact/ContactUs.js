ba7cdf567ff5b4d30c07bb3cd4887546ed0b68d3
Lindiks <lindsay.dickson@outlook.com>
2021-08-20 19:33:46 -0400

    Update ContactUs.js

--- client/src/components/LandingPages/Contact/ContactUs.js
+++ client/src/components/LandingPages/Contact/ContactUs.js
@@ -7,21 +7,21 @@ import ContactInfo from './ContactInfo'
 
 export default function ContactUs() {
     return (
-        <div className="contactBox px-5" >
-            <div className="flexAlignStart d-flex px-4 mb-5 mt-5 " >
+        <div className="contactBox px-5">
+            <div className="flexAlignStart d-flex px-4 mb-5 mt-5">
                 <h1 className="mcfH1"style={{color: '#004c54'}}>CONTACT US</h1>
             </div>
             <Form className="contactForm  d-flex flex-direction-row"> 
                 <div className="contactForm_col-1">
                     <Form.Group className="mb-5 px-4" controlId="formName">
-                        <Form.Label className="flexAlignStart d-flex" >Name</Form.Label>
+                        <Form.Label className="flexAlignStart d-flex">Name</Form.Label>
                         <Form.Control className="contactFormInputs" type="text" />
                     </Form.Group>
                     <Form.Group className="mb-5 px-4" controlId="formEmail">
                         <Form.Label className="flexAlignStart d-flex">Email</Form.Label>
                         <Form.Control className="contactFormInputs" type="email" />
                     </Form.Group>
-                    <Media queries={{ small: { maxWidth: 768 } }}>
+                    <Media queries={{ small: { maxWidth: 900 } }}>
                         {matches =>
                             matches.small ? (
                             <></>
@@ -37,7 +37,7 @@ export default function ContactUs() {
                         <Form.Label className="flexAlignStart d-flex">Inquiry</Form.Label>
                         <Form.Control className="contactFormInputs"  as="textarea" rows={5}/>{/* might want to switch from rows to height style tag  */} 
                     </Form.Group>
-                    <Media queries={{ small: { maxWidth: 768 } }}>
+                    <Media queries={{ small: { maxWidth: 900 } }}>
                         {matches =>
                             matches.small ? (
                             <ContactInfo />

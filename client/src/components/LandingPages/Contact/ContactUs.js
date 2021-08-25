587af7fae302788520a70f1f4ec6d4e6832939ac
Lindiks <lindsay.dickson@outlook.com>
2021-08-25 18:06:46 -0400

    Update ContactUs.js

--- client/src/components/LandingPages/Contact/ContactUs.js
+++ client/src/components/LandingPages/Contact/ContactUs.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Media from 'react-media'
 import '../../../css/main.css'
 import '../../../css/contact.css'
@@ -6,20 +6,54 @@ import {Form, Button} from 'react-bootstrap'
 import ContactInfo from './ContactInfo'
 
 export default function ContactUs() {
+    const [fname, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [inquiry, setInquiry] = useState('');
+    
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const form = event.currentTarget;
+        const name = form.name.value;
+        const email = form.email.value;
+        const inquiry = form.inquiry.value;
+        const formData = {
+            name,
+            email,
+            inquiry
+        }
+        fetch('sendEmail', {
+            method: 'POST',
+            body: formData,
+          });
+        console.log(formData);
+      };
+
+    // action="/sendEmail" method="POST"
+
     return (
         <div className="contactBox px-5">
             <div className="flexAlignStart d-flex px-4 mb-5 mt-5">
                 <h1 className="mcfH1"style={{color: '#004c54'}}>CONTACT US</h1>
             </div>
-            <Form action="/sendEmail" method="POST" className="contactForm  d-flex flex-direction-row"> 
+            <Form onSubmit={handleSubmit}  className="contactForm  d-flex flex-direction-row"> 
                 <div className="contactForm_col-1">
                     <Form.Group className="mb-5 px-4" controlId="formName">
                         <Form.Label className="flexAlignStart d-flex">Name</Form.Label>
-                        <Form.Control className="contactFormInputs" type="text" />
+                        <Form.Control className="contactFormInputs" type="text"  
+                    
+                        value={fname}
+                        name="name"
+                        onChange={event =>{ setName(event.target.value) }}
+           />
                     </Form.Group>
                     <Form.Group className="mb-5 px-4" controlId="formEmail">
                         <Form.Label className="flexAlignStart d-flex">Email</Form.Label>
-                        <Form.Control className="contactFormInputs" type="email" />
+                        <Form.Control className="contactFormInputs" type="email" required
+                        value={email}
+                        name="email"
+                        onChange={event =>{ setEmail(event.target.value) }}
+                        />
                     </Form.Group>
                     <Media queries={{ small: { maxWidth: 900 } }}>
                         {matches =>
@@ -35,7 +69,11 @@ export default function ContactUs() {
                 <div  className="contactForm_col-2">
                     <Form.Group className="mb-3 px-4" controlId="formInquiry">
                         <Form.Label className="flexAlignStart d-flex">Inquiry</Form.Label>
-                        <Form.Control className="contactFormInputs"  as="textarea" rows={5}/>{/* might want to switch from rows to height style tag  */} 
+                        <Form.Control className="contactFormInputs" as="textarea" 
+                         value={inquiry}
+                         name="inquiry"
+                         onChange={event =>{ setInquiry(event.target.value) }}
+                        rows={5}/>{/* might want to switch from rows to height style tag  */} 
                     </Form.Group>
                     <Media queries={{ small: { maxWidth: 900 } }}>
                         {matches =>
@@ -46,7 +84,7 @@ export default function ContactUs() {
                             )
                         }
                     </Media>
-                <Button variant="primary" className="buttonWhite"type="submit" >Submit</Button>
+                <Button className="buttonWhite" type="submit"  >Submit</Button>
                 {/* maybe display in place of the button after submit?  */}
                 <p style={{color: '#004c54'}}>Hang tight, we'll get back to you in 48 hours!</p>
                 </div>

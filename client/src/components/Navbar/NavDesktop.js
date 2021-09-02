8b5d9b6cc41acc2ea6bb02bc7f5ad830b8995d8e
Lindiks <lindsay.dickson@outlook.com>
2021-09-02 00:24:17 -0400

    Update NavDesktop.js

--- client/src/components/Navbar/NavDesktop.js
+++ client/src/components/Navbar/NavDesktop.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import {MenuItems} from "./MenuItems";
+import ScaleText from "react-scale-text";
+
 import 'react-bootstrap';
 import '../../css/Navbar.css';
 
@@ -14,6 +16,11 @@ class NavDesktop extends Component {
 // make mobile view as well w 3 bars + dropdown 
 
     render(){
+    const NavLi = (props) => (
+        <ScaleText minFontSize={24} maxFontSize={32} >
+          <span style={{color: '#004c54', textAlign:"start", fontWeight:'600'}}>{props.link}</span>
+        </ScaleText>
+      );
         return(
             <nav className="NavbarItems">
                 <h1 className="navbar-logo">Somethinf<i className="fab fa-react"></i></h1>
@@ -31,7 +38,8 @@ class NavDesktop extends Component {
                         return (
                             <li key={index}>
                                 <a className={item.cName} href={item.url}>
-                                {item.title}
+                                {/* {item.title} */}
+                                <NavLi link={item.title} />
                                 </a>
                             </li>
                         )

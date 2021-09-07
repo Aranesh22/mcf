8f364be72ee0bc1ad9262f18af9044368c4d182e
rishiekugathasan <rishie2001@gmail.com>
2021-09-06 20:10:42 -0400

    Revert "Revert "Merge branch 'main' of https://github.com/Aranesh22/mcf into main""
    This reverts commit b4338aa59b6d864d9db0c3964bdd717a13bdc0d6.

--- client/src/components/Footer/FooterDesktop.js
+++ client/src/components/Footer/FooterDesktop.js
@@ -5,7 +5,7 @@ import bigFooter from '../../assets/bigFooter.svg'
 export default function FooterDesktop() {
     return (
         <div className="fBg d-flex" style={{backgroundImage: `url(${bigFooter})`, position: 'absolute', flexDirection: 'column'}} >
-            {/* <div className="halfHexR1" style={{border: "solid red", width: '20%'}}>
+            <div className="halfHexR1" style={{border: "solid red", width: '20%'}}>
             </div>
             <div className="fullHexR2" style={{border: "solid blue",  marginLeft: '20%', width: '20%'}}>
             </div>
@@ -14,7 +14,7 @@ export default function FooterDesktop() {
             <div className="fullHexR2" style={{border: "solid orange", marginLeft: '60%',  width: '20%'}}>
             </div>
             <div className="fullHexR3" style={{border: "solid purple", marginLeft: '80%',  width: '20%'}}>
-            </div> */}
+            </div>
 
         </div>
     )

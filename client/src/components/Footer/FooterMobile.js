831fe241a23ee62280ccdaa2f94acaa9068d4e6e
Lindiks <lindsay.dickson@outlook.com>
2021-09-08 22:01:16 -0400

    Update FooterMobile.js

--- client/src/components/Footer/FooterMobile.js
+++ client/src/components/Footer/FooterMobile.js
@@ -7,12 +7,12 @@ export default function FooterMobile() {
             <img className="mb-5 mx-5" style={{width: '45%'}} src={mcfLogo} />
             <ul className="pagesList d-flex mx-5">
                 <li ><a href="../#">HOME</a></li>
-                <li><a href="../#">FIND YOUR PLAN</a></li>
-                <li><a href="../aboutUs">YOUR TEAM</a></li>
-                <li><a href="../contactUs">CONTACT US</a></li>
+                <li><a href="../#/quiz/question_1">KNOW YOUR STRENGTH</a></li>
+                <li><a href="../#/aboutUs">YOUR TEAM</a></li>
+                <li><a href="../#/contactUs">CONTACT US</a></li>
             </ul>
             <div className="socialsList">
-                f
+                
             </div>
         </div>
     )
